c3f9fad1b433ce7eb5713be7a69f70ec07505011
Rafid1077 <rafidkabir10@gmail.com>
2025-07-20 11:13:04 +0600

    Add files via upload

---
+++ lib/chat_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Chat")),
+      body: const Center(
+        child: Text(
+          "Chat feature under construction!",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
